319e46cedfbbfe677dbe6bfc85814018965135ad
Svlmon <titouan.foras@campus-igs-toulouse.fr>
2023-11-21 10:47:28 +0100

    Update BDD and create pokemonByID

--- api/main.py
+++ api/main.py
@@ -12,10 +12,20 @@ def read_root():
     return {"Hello": "World"}
 
 # Récupère la liste de tous les pokémons
-@app.get("/pokemons")
+@app.get("/api/pokemons")
 async def get_pokemons():
     query = "SELECT * FROM Pokemon"
     pokemons = await database.fetch_all(query)
     if not pokemons:
-        raise HTTPException(status_code=404, detail="No Pokémons found")
+        raise HTTPException(status_code=404, detail="Pas de pokemons trouvés")
     return pokemons
+
+@app.get("/api/pokemons/{pokemon_id}")
+async def get_pokemons_by_id(pokemon_id : int):
+    query = "SELECT * FROM Pokemon WHERE id_pokedex = :pokemon_id"
+    values = {"pokemon_id" : pokemon_id}
+    pokemons = await database.fetch_one(query, values)
+    if not pokemons:
+        raise HTTPException(status_code=404, detail="Pas de pokemons ayant cet ID")
+    return pokemons
+
